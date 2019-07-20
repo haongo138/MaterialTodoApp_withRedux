e812f668f533b632ddc533bc25cfacbc15a3abcd
Hao Ngo <haoxray@gmail.com>
2019-07-21 00:09:10 +0700

    Updates

---
+++ precache-manifest.13a59f0e0dc8292414185f8665dc1b39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32cbf9755b8e9b8e72e1b0985d27cb3",
+    "url": "/MaterialTodoApp_withRedux/index.html"
+  },
+  {
+    "revision": "44ba7747c4eb0994bdef",
+    "url": "/MaterialTodoApp_withRedux/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "ef682ef0becfb5b41b72",
+    "url": "/MaterialTodoApp_withRedux/static/css/main.08068b6f.chunk.css"
+  },
+  {
+    "revision": "44ba7747c4eb0994bdef",
+    "url": "/MaterialTodoApp_withRedux/static/js/2.5eb30802.chunk.js"
+  },
+  {
+    "revision": "ef682ef0becfb5b41b72",
+    "url": "/MaterialTodoApp_withRedux/static/js/main.d1f3427a.chunk.js"
+  },
+  {
+    "revision": "1b4b05a4764099821c34",
+    "url": "/MaterialTodoApp_withRedux/static/js/runtime~main.94d562fc.js"
+  }
+]);
